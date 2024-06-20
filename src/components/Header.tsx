5e14e84e630b26a284d88cc3ba8d92ff2ce051ea
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-06-20 06:00:11 +0300

    nearly there

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import "../index.css"; // Import the shared CSS file
 import arrow from "../assets/images/icon-arrow-dark.svg";
+import hamburger from "../assets/images/icon-hamburger.svg";
 const Header = () => {
   return (
     <header className="header">
@@ -8,24 +9,39 @@ const Header = () => {
         <nav>
           <ul>
             <li>
-              <a href="#about">
+              <a href="#product">
                 Product
                 <img src={arrow} className="arrow" alt="" />
               </a>
             </li>
+
             <li>
               <a href="#about">Company</a>
               <img src={arrow} className="arrow" alt="" />
             </li>
-            <li>
+            <li className="dropdown">
               <a href="#about">Connect</a>
               <img src={arrow} className="arrow" alt="" />
+              <ul className="dropdown-content">
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+                <li>
+                  <a href="#newsletter">Newsletter</a>
+                </li>
+                <li>
+                  <a href="#linkedin">LinkedIn</a>
+                </li>
+              </ul>
             </li>
           </ul>
         </nav>
         <div className="register">
-          <p>login</p>
-          <p className="sign">signin</p>
+          <p className="sign">login</p>
+          <p className="signs">signin</p>
+        </div>
+        <div className="icons">
+          <img src={hamburger} alt="" className="icon" />
         </div>
       </div>
       <div className="header-title">
